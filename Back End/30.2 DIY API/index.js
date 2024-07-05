5448294d3ac2fdd3c067b82171aeb3d4bab96bdd
Amit Singhal <rakshit.singhal712@gmail.com>
2024-07-05 17:26:08 +0530

    Get filter jokes

--- Back End/30.2 DIY API/index.js
+++ Back End/30.2 DIY API/index.js
@@ -21,6 +21,11 @@ app.get("/jokes/:id", (req, res) => {
 });
 
 //3. GET a jokes by filtering on the joke type
+app.get("/filter", (req, res) => {
+  const type = req.query.type;
+  const result = jokes.filter((joke) => joke.jokeType === type);
+  res.json(result);
+});
 
 //4. POST a new joke
 
